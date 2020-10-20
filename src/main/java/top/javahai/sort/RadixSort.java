709b8753b00bb2a9edc53912c8fa15c2ecc37cae
hai_global <jinhaihuang824@aliyun.com>
2020-10-21 00:18:34 +0800

    基数排序

--- src/main/java/top/javahai/sort/RadixSort.java
+++ src/main/java/top/javahai/sort/RadixSort.java
@@ -1,5 +1,9 @@
 package top.javahai.sort;
 
+import top.javahai.utils.DateUtil;
+
+import java.util.Date;
+
 /**
  * @author Hai
  * @program: DataStructure
@@ -7,6 +11,14 @@ package top.javahai.sort;
  * @create 2020/10/20 - 23:36
  **/
 public class RadixSort {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+        radixSort(arr);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+        testExecutionTime();
+    }
 
     /**
      * 基数排序是使用空间换取时间的经典算法
@@ -15,7 +27,7 @@ public class RadixSort {
      * 数组就有序了。
      * @param arr
      */
-    public void radixSort(int[] arr){
+    public static void radixSort(int[] arr){
         //1.获取数组中最大的数的位数,确定循环次数
         int max=arr[0];
         for (int i : arr) {
@@ -27,29 +39,46 @@ public class RadixSort {
         int maxLength=(max+"").length();
         //2.创建二维数组，其中x（一维数组）为10，表示1~9;y（每个一维数组的长度定为待排序数组的长度，因为不确定相同的数字有多少个）
         int[][] bucket=new int[10][arr.length];
-        //记录在每一个一维数组（桶）中存放的
+        //记录在每一个一维数组（桶）中存放的元素个数，用于标志一维数组（桶）元素可存放的位置
+        int[] buckElementCounts=new int[10];
         //3.遍历数组
         //3.1 第一层循环确定需要排序的次数
-        for (int i = 0,n=1; i < max; i++,n*=10) {
+        for (int i = 0,n=1; i < maxLength; i++,n*=10) {
             //3.2 第二层循环，进行一次排序，位数比较：个位->十位....
             for (int j = 0; j < arr.length; j++) {
                 //获取当前排序每个元素对应的位数
-                int digitOfElement=arr[i]/n%10;
+                int digitOfElement=(arr[j]/n)%10;
+                //根据位数，将元素放到桶的对应位置,然后+1
+                bucket[digitOfElement][buckElementCounts[digitOfElement]++]=arr[j];
+            }
+            //原数组下标
+            int index=0;
+            //一次排序完成将桶的元素按顺序放回到原本的数组中，完成一次排序
+            for (int k = 0; k < buckElementCounts.length; k++) {
+                if (buckElementCounts[k]!=0){
+//                    for (int num : bucket[k]) {这里不能直接赋值进去，因为0也会赋值会原数组
+//                       arr[index++]=num;
+//                    }
+                    for (int l = 0; l < buckElementCounts[k]; l++) {
+                        arr[index++]=bucket[k][l];
+
+                    }
+                }
+                //将buckElementCounts的记录初始化，进行下一次排序
+                buckElementCounts[k]=0;
             }
 
         }
+    }
+    public static void testExecutionTime(){
+        //生成测试数据，800000条
+        int[] arr = new int[800000];
         for (int i = 0; i < arr.length; i++) {
-
-
-
-
-
-
-         //将bucket桶中的数据依次放回到数组中，完成一次排序。
-
+            arr[i]=(int)(Math.random()*800000);
         }
-
-
+        System.out.println("当前时间："+ DateUtil.getCurrentTime(new Date()));
+        radixSort(arr);
+        System.out.println("排序完成时间："+DateUtil.getCurrentTime(new Date()));
 
     }
 }
