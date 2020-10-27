0af1de4168a400890293e5dac41a29c5b309fa07
hai_global <jinhaihuang824@aliyun.com>
2020-10-27 23:44:30 +0800

    线性查找、二分查找-递归实现

--- src/main/java/top/javahai/search/BinarySearch.java
+++ src/main/java/top/javahai/search/BinarySearch.java
@@ -1,5 +1,7 @@
 package top.javahai.search;
 
+import java.util.ArrayList;
+
 /**
  * @author Hai
  * @program: DataStructure
@@ -8,10 +10,14 @@ package top.javahai.search;
  **/
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr = {1, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9};
 
         System.out.println(binarySearch(arr, 0, arr.length - 1, 10));
         System.out.println(binarySearch(arr, 0, arr.length - 1, 8));
+
+        System.out.println(binarySearchV2(arr, 0, arr.length - 1, 3));
+        System.out.println(binarySearchV2(arr, 0, arr.length - 1, 7));
+        System.out.println(binarySearchV2(arr, 0, arr.length - 1, 710));
 
     }
 
@@ -43,5 +49,47 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    /**
+     * 找出数组中所有的等于目标数的下标
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @param target
+     * @return 使用ArrayList进行存储
+     */
+    public static ArrayList<Integer> binarySearchV2(int[] arr, int left, int right, int target) {
+        int mid = (left + right) / 2;
+        //终止条件1
+        if (arr[mid] == target) {
+            ArrayList<Integer> list = new ArrayList<>();
+            //向左查看是否有相等的数
+            int temp = mid;
+            while (temp >= 0 && arr[temp] == target) {
+                list.add(temp);
+                temp--;
+            }
+            temp = mid + 1;
+            //向右查看是否有相等的数
+            while (temp < arr.length && arr[temp] == target) {
+                list.add(temp);
+                temp++;
+            }
+            return list;
+        }
+        //终止条件2:找不到目标数，left
+        if (left >= right) {
+            return new ArrayList<Integer>();
+        }
+        if (arr[mid] > target) {
+            return binarySearchV2(arr, left, mid - 1, target);
+        }
+        if (arr[mid] <= target) {
+            return binarySearchV2(arr, mid + 1, right, target);
+        }
+        return new ArrayList<Integer>();
+    }
+
 
 }
--- src/main/java/top/javahai/search/SequenceSearch.java
+++ src/main/java/top/javahai/search/SequenceSearch.java
@@ -7,4 +7,18 @@ package top.javahai.search;
  * @create 2020/10/26 - 23:08
  **/
 public class SequenceSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(sequenceSearch(arr, 5));
+    }
+
+
+    public static int sequenceSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (target == arr[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
